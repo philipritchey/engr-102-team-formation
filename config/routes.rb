0d44bdad16c4bbb6e36c7ea7f034855f7d34a288
Raja karthik Vobugari <raju@tamu.edu>
2024-10-03 15:19:06 -0500

    Upload validation controller code added

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "/upload", to: "forms#upload"
+  post "validate_upload", to: "forms#validate_upload"
 
   # Defines the root path route ("/")
   # root "posts#index"
