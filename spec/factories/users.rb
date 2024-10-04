3a871f81d5b72f5893c122204efe86b81de32118
muresaijaideepreddy <saijaideepreddymure@tamu.edu>
2024-10-04 13:33:49 -0500

    bugs verified

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
       sequence(:email) { |n| "user#{n}@example.com" }
       name { "Test User" }
-      uin { "123456789" } 
+      uin { "123456789" }
       # Add other necessary attributes
     end
   end
