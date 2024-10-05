c41e1dca6a2565d9bf78f206cade787254af3bab
Raja karthik Vobugari <raju@tamu.edu>
2024-10-05 09:39:00 -0500

    changed oauth creds to env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,10 @@ require "jwt"
 Rails.application.config.middleware.use OmniAuth::Builder do
     begin
     # Retrieve the Google credentials from Rails credentials
-    google_credentials = Rails.application.credentials.google
+    # google_credentials = Rails.application.credentials.google
 
     # Configure the Google OAuth provider with the client_id and client_secret
-    provider :google_oauth2, google_credentials[:client_id], google_credentials[:client_secret], {
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
         scope: "email, profile", # This grants access to the user's email and profile information.
         prompt: "select_account", # This allows users to choose the account they want to log in with.
         image_aspect_ratio: "square", # Ensures the profile picture is a square.
