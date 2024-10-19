b435846baeca2daceb39c7e17b6fa239bf3e4681
Raja karthik Vobugari <raju@tamu.edu>
2024-10-18 19:56:31 -0500

    render bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,16 @@
 class WelcomeController < ApplicationController
-    skip_before_action :require_login, only: [ :index ]
-    def index
-      render layout: false
-        if logged_in?
-          if current_user
-            redirect_to user_path(current_user), notice: "Welcome, back!"
-          elsif current_student
-            redirect_to student_path(current_student), notice: "Welcome back, Student!"
-          end
-        else
-          # This will render the welcome page (index view)
-          # You can also add any logic to show on the welcome page
-        end
+  skip_before_action :require_login, only: [ :index ]
+
+  def index
+    if logged_in?
+      if current_user
+        redirect_to user_path(current_user), notice: "Welcome back!" and return
+      elsif current_student
+        redirect_to student_path(current_student), notice: "Welcome back, Student!" and return
       end
+    else
+      # This will render the welcome page (index view) without redirecting
+      render layout: false
+    end
+  end
 end
